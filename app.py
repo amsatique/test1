c8122789551291ea3976bf79f31b35e85000a313
amsatique <amsatique@users.noreply.github.com>
2022-01-12 14:51:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,27 @@ from elasticsearch import Elasticsearch
 import time
 import os
 
-# elk = 'https://elastic:5gCM65s1VT3PNasGq3f91G08@localhost:9201'
 elk = os.environ.get('ES_CONF')
-print(elk)
+
 try: 
     elk
 except NameError:
     exit
 
 def loop(es_url):
-    es = Elasticsearch([es_url], verify_certs=False, timeout=60, ssl_show_warn=False)
+    es = Elasticsearch([es_url], verify_certs=False, timeout=5, ssl_show_warn=False)
 
     res = es.search(index="employee", query={'match_all' : {}})
 
     for hit in res['hits']['hits']:
         print("%(first_name)s %(last_name)s: %(about)s" % hit["_source"])
     time.sleep(60)
-    
+    print("Waiting for next loop")
+        
 def main():
     print("Hello World!")
-    loop(elk)
+    while True:
+        loop(elk)
 
 if __name__ == "__main__":
     main()
